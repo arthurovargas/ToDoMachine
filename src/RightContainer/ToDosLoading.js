52b87c5eda2eb1e4363ff966901f095b9d680af6
ArthuroVargas <sergiovargas199791@gmail.com>
2024-12-04 23:22:47 -0500

    sKELETON 3

--- src/RightContainer/ToDosLoading.js
+++ src/RightContainer/ToDosLoading.js
@@ -1,6 +1,10 @@
 function ToDosLoading() {
   return (
-    <p>Cargando!!</p>
+    <div className="LoadingTodo-container">
+      <span className="LoadingTodo-completeIcon"></span>
+      <p className="LoadingTodo-text"></p>
+      <span className="LoadingTodo-deleteIcon"></span>
+    </div>
   )
 }
 
